10cc35f5a7970788e88ee2588cc3340f1bba29ce
Giacomo Marciani <giacomo.marciani@gmail.com>
2016-12-11 23:35:47 +0100

    license fix

--- src/main/java/com/acmutv/botnet/service/BotWrapperShutdown.java
+++ src/main/java/com/acmutv/botnet/service/BotWrapperShutdown.java
@@ -1,7 +1,7 @@
 /*
   The MIT License (MIT)
   <p>
-  Copyright (c) 2016 Giacomo Marciani, Michele Porretta
+  Copyright (c) 2016 Giacomo Marciani and Michele Porretta
   <p>
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
